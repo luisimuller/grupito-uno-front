0e9b2fff6e678cdb1eedbc6b81395ae6050ec58c
luisimuller <luisina.e.muller@gmail.com>
2025-10-13 23:49:14 -0300

    Se inicio con el proyecto y se trabajo principalmente sobre la bitacora

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ marginTop: "50px", textAlign: "center" }}>
+      <p>© 2025 Equipo NextHire Devs - Proyecto académico</p>
+    </footer>
+  );
+}
+
+export default Footer;
